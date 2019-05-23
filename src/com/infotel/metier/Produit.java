c00877c994db90ec14444b2688b602d8fd5cd150
Johan VAIRAC <johan.vairac@outlook.fr>
2019-05-23 16:08:42 +0200

    commit MAJ classe Produit

--- src/com/infotel/metier/Produit.java
+++ src/com/infotel/metier/Produit.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 
 import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.ManyToOne;
@@ -21,6 +24,9 @@ import javax.xml.bind.annotation.XmlSeeAlso;
 public abstract class Produit implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long idProduit;
 	private String nomProduit;
 	private int stock;
