66cb2a348e5647c5852c0baf623239751600d2c6
Remi dumarais <remi75010@gmail.com>
2019-05-23 17:12:47 +0200

    ejb

--- src/com/infotel/ejb/ProMagEjbImpl.java
+++ src/com/infotel/ejb/ProMagEjbImpl.java
@@ -15,64 +15,75 @@ public class ProMagEjbImpl implements IEjbLocal, IEjbRemote {
 	@EJB
 	private IdaoRemote dao;
 	
+	public IdaoRemote getDao() {
+		return dao;
+	}
+
+	public void setDao(IdaoRemote dao) {
+		this.dao = dao;
+	}
+
 	@Override
 	public void addProduit(Produit p) {
 		// TODO Auto-generated method stub
+		dao.addProduit(p);
 
 	}
 
 	@Override
 	public void removeProduit(long idProduit) {
 		// TODO Auto-generated method stub
+		dao.removeProduit(idProduit);
 
 	}
 
 	@Override
 	public Produit getProduit(long idProduit) {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.getProduit(idProduit);
 	}
 
 	@Override
 	public Produit editProduit(Produit p) {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.editProduit(p);
 	}
 
 	@Override
 	public List<Produit> getAllProduits() {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		return dao.getAllProduits();
+		}
 
 	@Override
 	public void addMagasin(Magasin m) {
 		// TODO Auto-generated method stub
-
+dao.addMagasin(m);
 	}
 
 	@Override
 	public void removeMagasin(long idMagasin) {
 		// TODO Auto-generated method stub
+		dao.removeMagasin(idMagasin);
 
 	}
 
 	@Override
 	public Magasin getMagasin(long idMagasin) {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.getMagasin(idMagasin);
 	}
 
 	@Override
 	public Magasin editMagasin(Magasin m) {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.editMagasin(m);
 	}
 
 	@Override
 	public List<Magasin> getAllMagasins() {
 		// TODO Auto-generated method stub
-		return null;
+		return dao.getAllMagasins();
 	}
 
 }
